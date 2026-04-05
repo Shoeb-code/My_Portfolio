d7ee35c6480be4376c5468357a66edc2ce878a89
Shoeb-code <shoebkhanhps143@gmail.com>
2026-04-05 14:22:41 +0530

    Fix case-sensitive imports for Vercel

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
-import Home from "./pages/Home/Home";
-import ProjectDetail from "./pages/Projects/ProjectDetail";
+import Home from "./pages/Home/Home.jsx";
+import ProjectDetail from "./pages/Projects/ProjectDetail.jsx";
 
 function App() {
   const location = useLocation();
